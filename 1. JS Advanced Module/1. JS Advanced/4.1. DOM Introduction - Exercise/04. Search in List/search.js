a81dfba2c63f3b4e84a832e2c69d69a090ee2ba4
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-22 15:48:13 +0300

    DOM introduction - Exercise

--- 1. JS Advanced Module/1. JS Advanced/4.1. DOM Introduction - Exercise/04. Search in List/search.js
+++ 1. JS Advanced Module/1. JS Advanced/4.1. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,13 @@
 function search() {
-   // TODO
+
+   let search = document.getElementById('searchText').textContent;
+   let arrayTown = document.getElementsByTagName('li');
+
+   for(let el of arrayTown) {
+
+      if(el.textContent === search) {
+         
+      }
+
+   }
 }
