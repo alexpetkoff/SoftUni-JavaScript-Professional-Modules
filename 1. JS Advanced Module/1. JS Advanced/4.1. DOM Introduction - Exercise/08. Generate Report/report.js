8cddcb1d2f04c08c56886c20d07ee8d6e41622df
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-24 12:47:52 +0300

    DOM Introduction - exercise

--- 1. JS Advanced Module/1. JS Advanced/4.1. DOM Introduction - Exercise/08. Generate Report/report.js
+++ 1. JS Advanced Module/1. JS Advanced/4.1. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,10 +1,18 @@
 function generateReport() {
-    let test = document.querySelectorAll('th input');
+    let checker = document.querySelectorAll('th input');
+    let rows = document.querySelectorAll('tr');
+    let output = document.getElementById('output');
+    let result = [];
 
-    for(let i = 0; i < test.length; i++) {
-        if(test[i].checked) {
-            console.log(test[i]);
+    for(let i = 1; i < rows.length; i++) {
+        let currentInfo = {};
+        for(let j = 0; j < checker.length; j++) {
+            if(checker[j].checked) {
+               currentInfo[checker[j].name] = rows[i].children[j].textContent;
+            }
         }
+        result.push(currentInfo);
     }
-
+    
+    output.value = JSON.stringify(result);
 }
