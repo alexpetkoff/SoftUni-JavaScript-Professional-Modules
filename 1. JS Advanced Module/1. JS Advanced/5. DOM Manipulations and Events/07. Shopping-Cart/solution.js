95278e4735c9885cf7ec9dee13b1cdfc26b57c04
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-26 10:33:14 +0300

    DOM Manipulations and Events

--- 1. JS Advanced Module/1. JS Advanced/5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 1. JS Advanced Module/1. JS Advanced/5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,18 +1,45 @@
 function solve() {
    let productsList = document.getElementsByClassName('product');
+   let checkOutButton = document.getElementsByClassName('checkout')[0];
+   checkOutButton.addEventListener('click', checkout);
 
-   for(let div of productsList) {
+   for(let div of Array.from(productsList)) {
       div.addEventListener('click', addElement);
    }
 
-   function addElement(event) {
-      let name;
-      if(event.target.classList[0] === 'add-product') {
-         console.log(productsList);
-      }
+   let totalPrice = 0;
+   let textArea = document.querySelector('textarea');
+   let text = ``;
+   let list = {};
 
-      // let button = event.target;
-      // console.log(button);
-      
+   function addElement(event) {
+      let select = event.target;
+      let parent = select.parentElement;
+      let name = parent.previousElementSibling.children[0].textContent;
+      let price = parent.nextElementSibling.textContent;
+   
+      if(event.target.classList[0] === 'add-product') {
+         price = +price;
+         totalPrice += price;
+         price = price.toFixed(2)
+         text += `Added ${name} for ${price} to the cart.\n`;
+         textArea.textContent = text;
+         list[name] = [];
+      }
+   }
+
+   function checkout(e) {
+      let products = [];
+      for(let key of Object.keys(list)) {
+         products.push(key);
+      }
+      let productsText = products.join(', ');
+      text += `You bought ${productsText} for ${totalPrice.toFixed(2)}.`;
+      textArea.textContent = text;
+
+      let allButtons = document.getElementsByTagName('button');
+      for(let button of allButtons) {
+         button.disabled = 'true';
+      }
    }
 }
