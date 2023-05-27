157b522530731e47ad298d55ca53acf61629492f
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-27 19:22:06 +0300

    DOM Manipulations and Events - Exercise

--- 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -23,23 +23,23 @@ function solve() {
         counter++;
         correctAnswers++;
         this.style.display = 'none';
-        this.nextElementSibling.style.display = 'block'
+        this.nextElementSibling.style.display = 'block';
       } else {
         counter++;
         this.style.display = 'none';
-        this.nextElementSibling.style.display = 'block'
+        this.nextElementSibling.style.display = 'block';
       }
     } 
     if(counter === 3) {
       let ul = document.getElementById('results');
-      ul.style.display = 'block'
+      ul.style.display = 'block';
       let li = ul.children[0];
       let h1 = li.children[0];
     
       if(correctAnswers === 3) {
-        h1.textContent = 'You are recognized as top JavaScript fan!'
+        h1.textContent = 'You are recognized as top JavaScript fan!';
       } else {
-        h1.textContent = `You have ${correctAnswers} right answers`
+        h1.textContent = `You have ${correctAnswers} right answers`;
       }
     }
   }
