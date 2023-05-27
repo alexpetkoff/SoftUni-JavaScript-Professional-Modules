3f84124c4ce3773ba15e424ae82dce17b24b43a2
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-27 15:37:57 +0300

    DOM Manipulations and Events - Exercise

--- 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', pressBtn);
+    });
+
+    function pressBtn(event) {
+
+        let button = event.target;
+        let parent = button.parentElement;
+        let radioBtn = parent.querySelector('[value="unlock"]');
+        
+        if(radioBtn.checked && button.textContent === "Show more"){
+            let hiddenDiv = button.previousElementSibling;
+            hiddenDiv.style.display = 'block';
+            button.textContent = "Hide it";
+            return;
+        } else if(radioBtn.checked && button.textContent === "Hide it"){
+            let hiddenDiv = button.previousElementSibling;
+            hiddenDiv.style.display = 'none';
+            button.textContent = "Show more";
+        }
+    }
 }
--- 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 1. JS Advanced Module/1. JS Advanced/5.1. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+    let dropDown = document.getElementById('menu');
+    
+    let newElement = document.createElement('option');
+    newElement.textContent = text.value;
+    newElement.value = value.value;
+    dropDown.appendChild(newElement);
+    
+    text.value = '';
+    value.value = '';
 }
