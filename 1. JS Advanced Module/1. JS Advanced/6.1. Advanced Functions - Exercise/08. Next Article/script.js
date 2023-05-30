30e6f06cb7ca78f33f4731cfb42b9dceaadc147e
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-30 22:27:08 +0300

    Advanced Functions - Exercise

--- 1. JS Advanced Module/1. JS Advanced/6.1. Advanced Functions - Exercise/08. Next Article/script.js
+++ 1. JS Advanced Module/1. JS Advanced/6.1. Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let index = 0;
+    return function () {
+        let div = document.getElementById('content');
+        let article = document.createElement('article');
+        let content = articles[index];
+        if (index < articles.length) {
+            let newArticle = div.appendChild(article);
+            newArticle.textContent = content;
+        }
+        index++;
+    }
 }
