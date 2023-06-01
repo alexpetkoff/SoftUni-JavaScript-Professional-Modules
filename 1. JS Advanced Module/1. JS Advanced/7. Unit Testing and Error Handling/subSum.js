4c3c8dfdf9751144e0ead2468e80cc0ca56cb2b7
Aleksandar Petkov <iskander9207@icloud.com>
2023-06-01 21:52:51 +0300

    Unit testing and Error Handling - Lab

---
+++ 1. JS Advanced Module/1. JS Advanced/7. Unit Testing and Error Handling/subSum.js
@@ -0,0 +1,26 @@
+function subSum(array, start, end) {
+
+    if(!Array.isArray(array)) {
+        return NaN;
+    }
+    if(start < 0) {
+        start = 0;
+    }
+    if(end > array.length - 1) {
+        end = array.length - 1;
+    }
+
+    let sum = 0;
+
+    for(let i = start; i <= end; i++) {
+        if(typeof(array[i]) === 'number') {
+            sum += array[i];
+        } else {
+            return NaN;
+        }
+    }
+
+    return sum;
+}
+
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300 ))
