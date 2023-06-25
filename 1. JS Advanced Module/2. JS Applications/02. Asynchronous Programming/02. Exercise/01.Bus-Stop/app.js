be71b19875378e46d5ea2f62b0b64142b47e8e17
Aleksandar Petkov <iskander9207@icloud.com>
2023-06-25 17:36:17 +0300

    Asynchronous Programming - Exercise

--- 1. JS Advanced Module/2. JS Applications/02. Asynchronous Programming/02. Exercise/01.Bus-Stop/app.js
+++ 1. JS Advanced Module/2. JS Applications/02. Asynchronous Programming/02. Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,36 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    
+    const input = document.getElementById('stopId');
+    const id = input.value;
+    const div = document.getElementById('stopName');
+    const ul = document.getElementById('buses');
+
+    await fetch(`http://localhost:3030/jsonstore/bus/businfo/${id}`)
+        .then(response => {
+
+            ul.innerHTML = '';
+            if(response.ok != true) {
+                throw new Error('Error');
+            }
+
+            return response.json();
+        })
+        .then(data => {
+
+            const buses = data.buses;
+            const name = data.name;
+            
+            div.textContent = `${name}`;
+            
+            for(let [busId, time] of Object.entries(buses)) {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                ul.appendChild(li);
+            }
+
+        })
+        .catch(error => {
+            div.textContent = 'Error';
+        });
+
 }
--- 1. JS Advanced Module/2. JS Applications/02. Asynchronous Programming/02. Exercise/02.Bus-Schedule/app.js
+++ 1. JS Advanced Module/2. JS Applications/02. Asynchronous Programming/02. Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,37 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let id = 'depot';
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let span = document.querySelector('.info');
+
+    async function depart() {
+
+        try {
+            const url = `http://localhost:3030/jsonstore/bus/schedule/${id}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            
+            let name = data.name;
+            let next = data.next;
+            id = next;
+            span.textContent = `Next stop ${name}`;
+
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+
+        } catch(error) {
+            span.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        span.textContent = span.textContent.replace('Next stop', 'Arriving at');
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
