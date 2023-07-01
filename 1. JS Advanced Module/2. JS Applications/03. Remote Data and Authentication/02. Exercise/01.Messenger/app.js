428c3a89ebf2a5cb5aab094cb7e2db9d2d1f1255
Aleksandar Petkov <iskander9207@icloud.com>
2023-07-01 15:37:39 +0300

    Remote Data and Authentication - Exercise

--- 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/02. Exercise/01.Messenger/app.js
+++ 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/02. Exercise/01.Messenger/app.js
@@ -1,5 +1,37 @@
 function attachEvents() {
-    console.log('TODO...');
+    
+    const submit = document.getElementById('submit');
+    submit.addEventListener('click', submitMessage);
+    const refresh = document.getElementById('refresh');
+    refresh.addEventListener('click', refreshMessages);
+
+    async function submitMessage(event) {
+        const name = document.querySelector('[name="author"]').value;
+        const message = document.querySelector('[name="content"]').value;
+
+        await fetch('http://localhost:3030/jsonstore/messenger', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                author: name,
+                content: message
+            })
+        });
+    }
+
+    async function refreshMessages(event) {
+        const textarea = document.querySelector('textarea');
+        const response = await fetch('http://localhost:3030/jsonstore/messenger');
+        const data = await response.json();
+        textarea.value = '';
+
+        const result = [];
+        Object.values(data).forEach(message => {
+            result.push(`${message.author}: ${message.content}`);
+        });
+        textarea.value = result.join('\n');
+    }
+
 }
 
 attachEvents();
--- 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/02. Exercise/02.Phonebook/app.js
+++ 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/02. Exercise/02.Phonebook/app.js
@@ -1,5 +1,62 @@
 function attachEvents() {
-    console.log('TODO...');
+    
+    const ul = document.getElementById('phonebook');
+    const loadBtn = document.getElementById('btnLoad');
+    const createBtn = document.getElementById('btnCreate');
+    loadBtn.addEventListener('click', loadPhonebook);
+    createBtn.addEventListener('click', createPhonebook);
+
+    async function loadPhonebook(event) {
+
+        const response = await fetch('http://localhost:3030/jsonstore/phonebook');
+        const data = await response.json();
+        ul.innerHTML='';
+        Object.values(data).forEach(e => {
+
+            const li = document.createElement('li');
+            const btn = document.createElement('button');
+            btn.textContent = 'Delete';
+            btn.addEventListener('click', deleteFunc);
+
+            li.textContent = `${e.person}: ${e.phone}`;
+            li.appendChild(btn);
+            ul.appendChild(li);
+
+            async function deleteFunc(event) {
+                const id = e._id;
+          
+                await fetch('http://localhost:3030/jsonstore/phonebook/' + id, {
+                    method: 'DELETE',
+                    headers: {'Content-Type': 'application/json'},
+                });
+
+                loadPhonebook();
+            }
+        })
+    }
+
+    async function createPhonebook(event) {
+
+        const personInput = document.getElementById('person');
+        const person = personInput.value;
+        const phoneInput = document.getElementById('phone');
+        const phone = phoneInput.value;
+
+        await fetch('http://localhost:3030/jsonstore/phonebook/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                person: person,
+                phone: phone
+            })
+        });
+        
+        personInput.value = '';
+        phoneInput.value = '';
+
+        loadPhonebook();
+    }
+
 }
 
 attachEvents();
