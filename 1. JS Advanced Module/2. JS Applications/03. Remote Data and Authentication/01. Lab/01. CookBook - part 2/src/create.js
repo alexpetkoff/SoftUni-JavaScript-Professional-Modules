82f140966b0357578e8337fa229668b9adb81c0e
Aleksandar Petkov <iskander9207@icloud.com>
2023-07-03 14:02:43 +0300

    Remote Data and Authentication - Lab
    Cookbook - part 3 is fully solved now and working.

--- 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/01. Lab/01. CookBook - part 2/src/create.js
+++ 1. JS Advanced Module/2. JS Applications/03. Remote Data and Authentication/01. Lab/01. CookBook - part 2/src/create.js
@@ -13,27 +13,26 @@ async function createRecipe(e) {
         let ingredients = formData.get('ingredients');
         let steps = formData.get('steps');
 
-        if(name = '' || img == '' || ingredients == '' || steps == '') {
+        if(name == '' || img == '' || ingredients == '' || steps == '') {
             throw new Error('Please, fill all fields!');
         }
 
         ingredients = ingredients.split('\n');
         steps = steps.split('\n');
-        console.log(name)
-        debugger;
-        // const res = await fetch('http://localhost:3030/data/recipes', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'X-Authorization': userData.token
-        //     },
-        //     body: JSON.stringify({
-        //         name: name,
-        //         img: img,
-        //         ingredients: ingredients,
-        //         steps: steps
-        //     })
-        // });
+
+        const res = await fetch('http://localhost:3030/data/recipes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Authorization': userData.token
+            },
+            body: JSON.stringify({
+                name: name,
+                img: img,
+                ingredients: ingredients,
+                steps: steps
+            })
+        });
        
         window.location = 'index.html';
 
