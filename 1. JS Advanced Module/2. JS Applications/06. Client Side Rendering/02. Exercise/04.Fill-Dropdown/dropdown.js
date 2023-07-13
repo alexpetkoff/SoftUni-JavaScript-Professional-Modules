d75020bcea49f9f0077d8bc5eac83356429fd297
Aleksandar Petkov <iskander9207@icloud.com>
2023-07-13 20:33:09 +0300

    Client Side Rendering - Exercise

--- 1. JS Advanced Module/2. JS Applications/06. Client Side Rendering/02. Exercise/04.Fill-Dropdown/dropdown.js
+++ 1. JS Advanced Module/2. JS Applications/06. Client Side Rendering/02. Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,5 +1,60 @@
-function addItem() {
+import { html, render } from './node_modules/lit-html/lit-html.js';
 
-    
-
+async function fetchItems() {
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown');
+    const data = await response.json();
+    return data;
 }
+
+function renderItems(items) {
+    const menu = document.getElementById('menu');
+    render(
+        html`
+        ${Object.values(items).map((item) => html`<option value="${item._id}">${item.text}</option>`)}
+      `,
+        menu
+    );
+}
+
+async function addItem(itemText) {
+    if(itemText === '') {
+        return;
+    }
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: itemText })
+    });
+
+    if (response.ok) {
+        const newItem = await response.json();
+        return newItem;
+    } else {
+        throw new Error('Failed to add item');
+    }
+}
+
+document.querySelector('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const itemInput = document.getElementById('itemText');
+    const newItemText = itemInput.value;
+
+    try {
+        const newItem = await addItem(newItemText);
+        itemInput.value = '';
+        renderItems(await fetchItems());
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+(async () => {
+    try {
+        const items = await fetchItems();
+        renderItems(items);
+    } catch (error) {
+        console.error(error);
+    }
+})();
