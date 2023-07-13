38990f530737a96b1d436e7131acc83082057209
Aleksandar Petkov <iskander9207@icloud.com>
2023-07-13 22:05:45 +0300

    Client Side Rendering - exercise

--- 1. JS Advanced Module/2. JS Applications/06. Client Side Rendering/02. Exercise/05.Table-Search/solution.js
+++ 1. JS Advanced Module/2. JS Applications/06. Client Side Rendering/02. Exercise/05.Table-Search/solution.js
@@ -1,8 +1,47 @@
+import { html , render } from './node_modules/lit-html/lit-html.js'
+
+renderTable();
+solve();
+
+async function renderTable() {
+
+   const tbody = document.querySelector('tbody')
+   const response = await fetch('http://localhost:3030/jsonstore/advanced/table');
+   const data = await response.json();
+
+   const template = html`
+         ${Object.values(data).map((p) => {
+           return html`
+            <tr>
+                <td>${p.firstName} ${p.lastName}</td>
+                <td>${p.email}</td>
+                <td>${p.course}</td>
+            </tr>`
+         })}`;
+
+   render(template, tbody);
+
+}
+
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   function onClick(e) {
+      const value = e.target.previousElementSibling.value.toLowerCase();
+      const tdValues = document.querySelectorAll('td');
 
+      Array.from(tdValues).forEach(td => {
+         const tr = td.parentElement;
+         tr.classList.remove('select');
+      });
+
+      Array.from(tdValues).forEach(td => {
+         let tdVal = td.textContent.toLowerCase();
+         const tr = td.parentElement;
+
+         if(tdVal.includes(value)) {
+            tr.classList.add('select');
+         }
+      });
    }
 }
