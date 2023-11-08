586f1296f1494748dd08867fb035be62aca4fd86
Alexander Petkov <iskander9207@icloud.com>
2023-11-08 14:56:39 +0200

    Workshop - User List
    Initialized the project template.

--- 3. ReactJS/4. Workshop - User List/client/src/App.jsx
+++ 3. ReactJS/4. Workshop - User List/client/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import '../styles.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Header />
         <main className="main">
-          
+
         </main>
         <Footer />
     </div>
