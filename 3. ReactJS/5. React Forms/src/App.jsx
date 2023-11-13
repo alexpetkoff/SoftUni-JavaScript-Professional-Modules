7de18315394d1cfe1155d660303604f10920ee50
Alexander Petkov <iskander9207@icloud.com>
2023-11-13 17:31:42 +0200

    Update React Forms
    -added example with controlled forms

--- 3. ReactJS/5. React Forms/src/App.jsx
+++ 3. ReactJS/5. React Forms/src/App.jsx
@@ -1,11 +1,13 @@
 import { UncontrolledForm } from './components/UncontrolledForm'
 import './App.css'
+import { ControlledForm } from './components/ControlledForm';
 
 function App() {
 
   return (
     <>
       <UncontrolledForm />
+      <ControlledForm />
     </>
   )
 }
---
+++ 3. ReactJS/5. React Forms/src/components/ControlledForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export const ControlledForm = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+    })
+
+    const onFormChange = (e) =>{
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+
+        
+    }
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+
+        console.log(user);
+    }
+
+    return (
+        <>
+            <h1>Controlled Form:</h1>
+            <h3>Register Form</h3>
+
+            <form onSubmit={onSubmitForm}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input onChange={onFormChange} value={user.username} type="text" name="username" id="usernameControlled" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input onChange={onFormChange} value={user.password} type="password" name="password" id="passwordControlled" />
+                </div>
+                <div>
+                    <input type="submit" value="Register" />
+                </div>
+            </form>
+        </>
+    );
+}
