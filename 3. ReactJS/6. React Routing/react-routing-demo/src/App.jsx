2108894c6e82e2cfb3134c83b959fc72eadc236d
Alexander Petkov <iskander9207@icloud.com>
2023-11-14 16:38:00 +0200

    Created new project - React Routing Demo
    Initialized the project

---
+++ 3. ReactJS/6. React Routing/react-routing-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navigation } from './Navigation';
+import './App.css';
+
+function App() {
+
+	return (
+		<>
+			<h1>React Router</h1>
+			<Navigation />
+			<footer>All rights reserver &copy;</footer>
+		</>
+	)
+}
+
+export default App
---
+++ 3. ReactJS/6. React Routing/react-routing-demo/src/Navigation.jsx
@@ -0,0 +1,9 @@
+export const Navigation = () => {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/Catalog">Catalog</a>
+        </nav>
+    );
+}
