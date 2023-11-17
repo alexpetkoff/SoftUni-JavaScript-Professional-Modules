fd68edbd36d08f60cf516968421694b784fb18be
Alexander Petkov <iskander9207@icloud.com>
2023-11-17 16:45:05 +0200

    Update NavBar.jsx
    added functionality for <hr /> tag - show / hide

--- 3. ReactJS/Project Defense - React/client/src/components/navbar/Navbar.jsx
+++ 3. ReactJS/Project Defense - React/client/src/components/navbar/Navbar.jsx
@@ -2,7 +2,12 @@ import styles from './Navbar.module.css';
 import logo from '../../assets/logo.png';
 import cart from '../../assets/cart-icon.png';
 
+import { useState } from 'react';
+
 export default function Navbar() {
+
+    const [clicked, setClicked] = useState('home');
+
     return (
         <div className={styles.navbar}>
             <div className={styles['nav-logo']}>
@@ -10,15 +15,15 @@ export default function Navbar() {
                 <p>Apple Store</p>
             </div>
             <ul className={styles['nav-menu']}>
-                <li>Home <hr /></li>
-                <li>iPhone</li>
-                <li>Macbook</li>
-                <li>Mac</li>
-                <li>Mac Studio</li>
+                <li onClick={() => setClicked('home')}>Home {clicked === 'home' ? <hr /> : <></>}</li>
+                <li onClick={() => setClicked('iphone')}>iPhone {clicked === 'iphone' ? <hr /> : <></>}</li>
+                <li onClick={() => setClicked('macbook')}>Macbook {clicked === 'macbook' ? <hr /> : <></>}</li>
+                <li onClick={() => setClicked('mac')}>Mac {clicked === 'mac' ? <hr /> : <></>}</li>
+                <li onClick={() => setClicked('macstudio')}>Mac Studio {clicked === 'macstudio' ? <hr /> : <></>}</li>
             </ul>
             <div className={styles['nav-cart-login']}>
                 <button>Login</button>
-                <img src={cart} alt="shopping-cart"/>
+                <img src={cart} alt="shopping-cart" />
                 <div className={styles['cart-count']}>0</div>
             </div>
         </div>
