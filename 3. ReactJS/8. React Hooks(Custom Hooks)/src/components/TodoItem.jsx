e85092b9a42bc83f351fe00de9d1c4c8e4722414
Alexander Petkov <iskander9207@icloud.com>
2023-11-22 17:31:31 +0200

    Update TodoItem.jsx
    added button X to delete todos

--- 3. ReactJS/8. React Hooks(Custom Hooks)/src/components/TodoItem.jsx
+++ 3. ReactJS/8. React Hooks(Custom Hooks)/src/components/TodoItem.jsx
@@ -1,7 +1,10 @@
+import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-export default function TodoItem({text, isComplete}) {
+export default function TodoItem({ text, isComplete }) {
     return (
-        <ListGroup.Item>{text} ---- Completed: {isComplete === false ? "No" : "Yes"}</ListGroup.Item>
+            <ListGroup.Item>{text} ---- Completed: {isComplete === false ? "No" : "Yes"}
+                <Button style={{marginLeft: "20px"}}>X</Button>
+            </ListGroup.Item>
     );
 }
