e7cdf2a572364b292ed91a542fb134ae753c5161
Alexander Petkov <iskander9207@icloud.com>
2023-11-23 17:41:52 +0200

    Create controll login form

--- 3. ReactJS/9. Workshop - React Hooks and Authentication/client/src/components/login/Login.jsx
+++ 3. ReactJS/9. Workshop - React Hooks and Authentication/client/src/components/login/Login.jsx
@@ -1,17 +1,24 @@
+import useForm from '../../hooks/useForm';
 import { Link } from 'react-router-dom';
 
-export default function Login() {
+export default function Login({loginSubmitHandler}) {
+
+    const {values, onChange, onSubmit} = useForm(loginSubmitHandler, {
+        email: '',
+        password: ''
+    });
+
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input value={values['email']} onChange={onChange} type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
 
                     <label htmlFor="login-password">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input value={values['password']} onChange={onChange} type="password" id="login-password" name="password" />
 
                     <input type="submit" className="btn submit" value="Login" />
 
