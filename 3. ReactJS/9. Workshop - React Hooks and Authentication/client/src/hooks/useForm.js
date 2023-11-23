9f7578132087107db971a8471312b2490475dc66
Alexander Petkov <iskander9207@icloud.com>
2023-11-23 17:42:15 +0200

    Created useForm custom hook to control the Login form

--- 3. ReactJS/9. Workshop - React Hooks and Authentication/client/src/hooks/useForm.js
+++ 3. ReactJS/9. Workshop - React Hooks and Authentication/client/src/hooks/useForm.js
@@ -1,3 +1,21 @@
-export default function useForm() {
+import { useState, useEffect } from 'react';
 
+export default function useForm(loginSubmitHandler, initialValues) {
+    const [values, setValues] = useState(initialValues);
+
+    const onChange = (e) => {
+        setValues((values) => ({...values, [e.target.name]: e.target.value}));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        loginSubmitHandler(values);
+    }
+
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
 }
