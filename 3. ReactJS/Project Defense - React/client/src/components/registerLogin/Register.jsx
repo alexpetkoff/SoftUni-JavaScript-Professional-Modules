4771ffaa68f0f50b60a85679367ae78ceefa21cd
Alexander Petkov <iskander9207@icloud.com>
2023-12-07 17:53:23 +0200

    error handling for register

--- 3. ReactJS/Project Defense - React/client/src/components/registerLogin/Register.jsx
+++ 3. ReactJS/Project Defense - React/client/src/components/registerLogin/Register.jsx
@@ -13,15 +13,36 @@ function Register() {
     password: "",
   });
 
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     const { username, email, password } = formData;
+
+    if (!username || !email || !password) {
+      setErrors({
+        username: !username ? "Please enter your name." : "",
+        email: !email ? "Please enter your email." : "",
+        password: !password ? "Please enter your password." : "",
+      });
+      return;
+    }
+
     const response = await registerSubmitHandler({ email, password, username });
   };
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
   };
 
   return (
@@ -35,20 +56,24 @@ function Register() {
             name="username"
             placeholder="Your Name..."
           />
+          <div className="error">{errors.username}</div>
           <input
             onChange={onChangeHandler}
             type="email"
             name="email"
             placeholder="Your Email..."
           />
+          <div className="error">{errors.email}</div>
           <input
             onChange={onChangeHandler}
             type="password"
             name="password"
             placeholder="Your Password..."
           />
+          <div className="error">{errors.password}</div>
         </div>
         <input className="btn-submit" type="submit" value="Register" />
+        <div className="error">{errors.server}</div>
         <p className="loginsignup-login">
           Already have an account?{" "}
           <Link to="/login">
