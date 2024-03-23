9ea0c6f820d7371328cbec2d27d3a18055ae32c7
Alexander Petkov <iskander9207@icloud.com>
2024-03-23 19:51:01 +0200

    added increment and decrement functionalities to the buttons

--- Diving into React Redux + Toolkit/Redux Counter/src/components/Counter.js
+++ Diving into React Redux + Toolkit/Redux Counter/src/components/Counter.js
@@ -1,9 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+    const dispatch = useDispatch();
     const counter = useSelector((state) => state.counter);
+
+    const incrementHandler = () => {
+        dispatch({ type: "increment" });
+    };
+
+    const decrementHandler = () => {
+        dispatch({ type: "decrement" });
+    };
 
     const toggleCounterHandler = () => {};
 
@@ -12,8 +21,8 @@ const Counter = () => {
             <h1>Redux Counter</h1>
             <div className={classes.value}>{counter}</div>
             <div>
-                <button>Increment</button>
-                <button>Decrement</button>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={decrementHandler}>Decrement</button>
             </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
