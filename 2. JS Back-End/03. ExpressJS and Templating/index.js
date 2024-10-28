dc6dd7b56cad730e9424b8e721f8477236bd2b41
alexPetkov92 <iskander9207@icloud.com>
2024-10-28 17:31:30 +0200

    middlewares and build in middlewares.

--- 2. JS Back-End/03. ExpressJS and Templating/index.js
+++ 2. JS Back-End/03. ExpressJS and Templating/index.js
@@ -3,13 +3,18 @@ const app = express();
 
 const port = 3000;
 
+const bodyParser = express.urlencoded({ extended: false })
+app.use(bodyParser)
+
 // #region Routing
 app.get("/", (req, res) => {
     res.status(200);
     res.send("Welcome to ExpressJS");
 });
 
-app.post("/", (req, res) => {
+
+app.post("/kittens", (req, res) => {
+    console.log(req.body.name) // now we can read the json because of the bodyParser middleware
     res.send("Welcome to ExpressJS - Post request");
 });
 
@@ -18,9 +23,24 @@ app.post("/", (req, res) => {
 // "/kittens" - path / route
 // action = (req, res) => {}
 app.get("/kittens", (req, res) => {
-
-    res.send('This is kittens page!')
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post">
+            <label for="name">Name:</label>
+            <input name="name" type="text" id="name" />
+            <input type="submit" />
+        </form>
+    </body>
+    <script>console.log('Loading from sent html!')</script>
+</html>`)
 });
+
 
 app.get("/kittens/:kittenId", (req, res) => {
 
