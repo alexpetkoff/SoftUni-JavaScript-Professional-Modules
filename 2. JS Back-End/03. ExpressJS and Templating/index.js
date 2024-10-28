3cd656a2c1a2082d9bfc72f3ab05cae41d27fee3
alexPetkov92 <iskander9207@icloud.com>
2024-10-28 14:37:33 +0200

    Update index.js

--- 2. JS Back-End/03. ExpressJS and Templating/index.js
+++ 2. JS Back-End/03. ExpressJS and Templating/index.js
@@ -7,21 +7,30 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.status(200);
     res.send("Welcome to ExpressJS");
-})
+});
+
+app.post("/", (req, res) => {
+    res.send("Welcome to ExpressJS - Post request");
+});
 
 // Endpoint
 // get - method
 // "/kittens" - path / route
 // action = (req, res) => {}
 app.get("/kittens", (req, res) => {
-    res.status(200);
-    res.send("Welcome to ExpressJS - Kittens Page");
-})
+
+    res.send('This is kittens page!')
+});
+
+app.get("/kittens/:kittenId", (req, res) => {
+
+    res.send(`This is kitten with id: ${req.params.kittenId}`)
+});
 
 // * route must be always at the bottom!!!
 app.get("*", (req, res) => {
     res.send("PAGE NOT FOUND!");
-})
+});
 // #endregion
 
 app.listen(port, () => console.log(`Express is running on port: ${port}`));
