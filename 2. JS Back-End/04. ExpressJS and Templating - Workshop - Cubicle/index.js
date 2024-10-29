f84d61934cc7557d62e75d8e916ba5eccb08dea1
alexPetkov92 <iskander9207@icloud.com>
2024-10-29 16:02:13 +0200

    Update index.js

--- 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/index.js
+++ 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/index.js
@@ -7,10 +7,10 @@ const PORT = 3000;
 //handlebars config
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
-app.set("views", "views")
+app.set("views", "views");
 
 app.get("/", (req, res) => {
     res.render("index", {});
 });
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`))
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`));
