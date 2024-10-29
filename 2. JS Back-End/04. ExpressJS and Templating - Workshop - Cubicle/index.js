085ac566ba47bec40b84b1c8252ecba9d20e8f6a
alexPetkov92 <iskander9207@icloud.com>
2024-10-29 15:21:51 +0200

    basic app config + added gitignore

--- 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/index.js
+++ 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/index.js
@@ -1,9 +1,9 @@
-const env = process.env.NODE_ENV || 'development';
+const express = require("express")
+const app = express();
+const PORT = 3000;
 
-const config = require('./config/config')[env];
-const app = require('express')();
+app.get('/', (req, res) => {
+    res.send('<h2>Hello from ExpressJS!</h2>')
+})
 
-require('./config/express')(app);
-require('./config/routes')(app);
-
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`))
