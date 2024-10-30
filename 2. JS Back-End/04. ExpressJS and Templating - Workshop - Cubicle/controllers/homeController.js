f78d780ee6b6d9e54651a048042f133ad73e9469
alexPetkov92 <iskander9207@icloud.com>
2024-10-30 17:20:18 +0200

    Update homeController.js

--- 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/controllers/homeController.js
+++ 2. JS Back-End/04. ExpressJS and Templating - Workshop - Cubicle/controllers/homeController.js
@@ -4,4 +4,13 @@ router.get("/", (req, res) => {
     res.render('partials/index')
 })
 
+router.get("/about", (req, res) => {
+    res.render('partials/about')
+})
+
+router.get("/create", (req, res) => {
+    res.render('partials/create')
+})
+
+
 module.exports = router;
